1c600b62603ad17706a7d03d4b1e72a08eda5849
Dave Guymon <davidwguymon@gmail.com>
2017-07-03 18:24:14 -0600

    Completed Step 2

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EvenAndOdd extends Component {
+    render() {
+        return (
+        
+            <p>EvenAndOdd Component</p>
+            
+        );
+    }
+}
+
+export default EvenAndOdd;
---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class FilterObject extends Component {
+
+    render() {
+        return ( <
+            p > FilterObjects Component < /p>
+        )
+    }
+}
+
+export default FilterObject;
